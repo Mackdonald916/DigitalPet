bab24601454ab1d71e97ca60f1d15d6814e63aa9
Mackdonald916 <shiwans352@gmail.com>
2025-04-18 22:32:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from pet import Pet
+
+# Create a pet instance
+my_pet = Pet("Buddy")
+
+# Interact with the pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("roll over")
+my_pet.train("sit")
+
+# Show learned tricks
+my_pet.show_tricks()
+
+# Final status
+my_pet.get_status()
